ded4f29b4f00be7ac08130fbbeae7fb5300d20c4
TheJeme <eemelijoonatan@gmail.com>
2021-03-06 14:11:04 +0200

    Added navigation with pages

--- Markem/Markem/MainPage.xaml.cs
+++ Markem/Markem/MainPage.xaml.cs
@@ -14,5 +14,30 @@ namespace Markem
         {
             InitializeComponent();
         }
+
+        private void ReactionButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ReactionTest());
+        }
+        private void TimeButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TimeTest());
+        }
+        private void NumberButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NumberTest());
+        }
+        private void VerbalButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new VerbalTest());
+        }
+        private void VisualButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new VisualTest());
+        }
+        private void SequenceButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SequenceTest());
+        }
     }
 }
