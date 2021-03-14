b0e98e9dd01e5239bc8cc7cd8f8b6705c682b839
TheJeme <eemelijoonatan@gmail.com>
2021-03-14 22:19:36 +0200

    VisualTest UI improved

--- Markem/Markem/VisualTest.xaml.cs
+++ Markem/Markem/VisualTest.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -17,9 +14,147 @@ namespace Markem
             InitializeComponent();
         }
 
+        // Events
+
         private void OnPlayTapped(object sender, EventArgs e)
         {
 
         }
+
+        private void TryAgainButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board00Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board01Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board02Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board03Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board04Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board10Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board11Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board12Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board13Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board14Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board20Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board21Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board22Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board23Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board24Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board30Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board31Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board32Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board33Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board34Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board40Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void Board41Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board42Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board43Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board44Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        // Functions
     }
 }
